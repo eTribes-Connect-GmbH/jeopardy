40e613fcf70273d9c9648bc418756e3b972f59a4
benjamin.schnoor <benjamin.schnoor@etribes.de>
2020-05-21 15:59:07 +0200

    try publish

--- ci/pipelines/build/docker/build.sh
+++ ci/pipelines/build/docker/build.sh
@@ -6,7 +6,7 @@ SCRIPT_PATH="$(realpath "$(dirname "${0}")")"
 PROJECT_ROOT="$(realpath "${SCRIPT_PATH}/../../../..")"
 TAG="${CI_COMMIT_REF_NAME}"
 REGISTRY="${CI_DOCKER_REGISTRY_URL}"
-NAME="${CI_PROJECT_NAME}-skill"
+NAME="${CI_PROJECT_NAME}-shopware"
 
 echo "${CI_DOCKER_REGISTRY_PASSWORD}" | docker login "${REGISTRY}" --username="${CI_DOCKER_REGISTRY_USER}" --password-stdin
 docker build -f ci/pipelines/build/docker/Dockerfile "${PROJECT_ROOT}" -t "${REGISTRY}/${NAME}:${TAG}"
